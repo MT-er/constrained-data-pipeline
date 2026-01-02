9a89dcafcf750a7f9b08d1492bfd6b9216de6926
Meryem Trichni <178589092+MT-er@users.noreply.github.com>
2026-01-02 22:49:52 +0100

    Change log input file from logs.csv to logs.txt

--- preprocess.sh
+++ preprocess.sh
@@ -2,7 +2,7 @@
 
 # Deriving a Comma Separated File from the raw logs sample
 echo 'ip,path,status' > cleaned_logs.csv
-awk '{print $1","$9","$11}' logs.csv >> cleaned_logs.csv
+awk '{print $1","$9","$11}' logs.txt >> cleaned_logs.csv
 
 # Counts unique status codes
 awk -F',' 'NR>1 {print $3}' cleaned_logs.csv | sort | uniq -c >> status_count0.txt
